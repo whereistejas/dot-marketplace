0105317c96f4d4c29f95601b103b156ddace8f3c
Tejas Sanap <sanap.tejas@gmail.com>
2021-09-07 02:06:01 +0530

    Rename template pallet to tasking pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -40,8 +40,8 @@ use pallet_transaction_payment::CurrencyAdapter;
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-/// Import the template pallet.
-pub use pallet_template;
+/// Import the tasking pallet.
+pub use pallet_tasking;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -267,7 +267,7 @@ impl pallet_sudo::Config for Runtime {
 }
 
 /// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl pallet_tasking::Config for Runtime {
 	type Event = Event;
 }
 
@@ -287,7 +287,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment::{Pallet, Storage},
 		Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+		TaskingModule: pallet_tasking::{Pallet, Call, Storage, Event<T>},
 	}
 );
 
