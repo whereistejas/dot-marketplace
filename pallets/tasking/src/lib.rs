3e6853ae04d3a3ed2e0bba435430490f662cea0a
Tejas Sanap <sanap.tejas@gmail.com>
2021-09-14 13:45:10 +0530

    Add dispatchable methods

--- pallets/tasking/src/lib.rs
+++ pallets/tasking/src/lib.rs
@@ -1,103 +1,98 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
-pub use pallet::*;
-
 #[cfg(test)]
 mod mock;
 
 #[cfg(test)]
 mod tests;
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
+// Re-export pallet items so that they can be accessed from the crate namespace.
+pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
+	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
+	type TaskId = u32;
+
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[pallet::event] // <-- Step 3. code block will replace this.
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	#[pallet::metadata(T::AccountId = "AccountId")]
+	pub enum Event<T: Config> {
+		/// Create new task. \[who, task-id\]
+		NewTask(T::AccountId, TaskId),
+		/// Remove existing task. \[who, task-id\]
+		RemoveTask(T::AccountId, TaskId),
+		/// Get all task Ids \[task-id\]
+		GetTask(Vec<TaskId>),
+	}
+
+	#[pallet::error] // <-- Step 4. code block will replace this.
+	pub enum Error<T> {
+		/// Duplicate tasks cannot exist.
+		TaskAlreadyExists,
+		/// Task does not exist.
+		TaskDoesNotExist,
+		/// WrongTaskOwner
+		TaskWrongOwner,
 	}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::storage] // <-- Step 5. code block will replace this.
+	pub(super) type Tasks<T: Config> =
+		StorageMap<_, Blake2_128Concat, TaskId, (T::AccountId, T::BlockNumber), ValueQuery>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/events
-	#[pallet::event]
-	#[pallet::metadata(T::AccountId = "AccountId")]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
-	}
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-	// Errors inform users that something went wrong.
-	#[pallet::error]
-	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
-	}
-
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
-	#[pallet::call]
+	#[pallet::call] // <-- Step 6. code block will replace this.
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
+		#[pallet::weight(1_000)]
+		pub fn create_task(origin: OriginFor<T>, task: TaskId) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+			ensure!(!Tasks::<T>::contains_key(&task), Error::<T>::TaskAlreadyExists);
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
+			let current_block = <frame_system::Pallet<T>>::block_number();
+
+			Tasks::<T>::insert(&task, (&sender, current_block));
+
+			Self::deposit_event(Event::NewTask(sender, task));
+
+			Ok(().into())
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		#[pallet::weight(1_000)]
+		pub fn remove_task(origin: OriginFor<T>, task: TaskId) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+			ensure!(Tasks::<T>::contains_key(&task), Error::<T>::TaskDoesNotExist);
+
+			let (owner, _) = Tasks::<T>::get(&task);
+
+			ensure!(sender == owner, Error::<T>::TaskWrongOwner);
+
+			Self::deposit_event(Event::RemoveTask(sender, task));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(1_000)]
+		pub fn get_task(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+			let taskid: Vec<_> = Tasks::<T>::iter().map(|(key, ..)| key).collect();
+
+			Self::deposit_event(Event::GetTask(taskid));
+
+			Ok(().into())
 		}
 	}
 }
